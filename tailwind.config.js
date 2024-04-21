f6e6e2fa11a50607b01549c71288b92c4e59a322
Jesse Guenzl <contact@jesseguenzl.com>
2024-04-21 20:28:07 +0200

    copy tailwind config from old project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,103 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      // padding: "2rem",
+      // screens: {
+      //   "2xl": "1400px",
+      // },
+    },
+    fontFamily: {
+      sans: ['var(--font-inter)', ...fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        // "off-white": "hsl(var(--card))",
+        // "off-white": "#FBFBFB",
+        yellow: {
+          // 200: "#FCF0C4", // beige
+          600: '#F8D600', // yellow light
+          // 700: "#EEC211", // yellow dark
+        },
+        gray: {
+          50: '#F6F6F6',
+          100: '#ECECEC',
+          200: '#CDCDCC',
+          300: '#B0B0AF',
+          400: '#868685',
+          500: '#757574',
+          600: '#575756',
+          900: '#20201E',
+        },
+        offwhite: '#FBFBFB',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+          footer: 'hsl(var(--card-footer))',
+        },
+      },
+      gridTemplateColumns: {
+        sidebar: '256px auto', //for sidebar layout
+      },
+      gridTemplateRows: {
+        header: '56px auto', //for the navbar layout
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
   },
   plugins: [],
-}
-
+};
