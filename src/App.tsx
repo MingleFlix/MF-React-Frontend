20b4d3f517f7a4a6c18edf3aa3a17f58684844ba
Jesse Guenzl <contact@jesseguenzl.com>
2024-04-21 18:17:23 +0200

    create HomePage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./components/common/Header/Header.tsx";
-import HeroSection from "./components/layout/HeroSection/HeroSection.tsx";
+import {HomePage} from "./pages/HomePage.tsx";
 
 function App() {
   return (
     <Router>
         <Header />
         <Routes>
-            <Route path="/" element={<HeroSection />} />
+            <Route path="/" element={<HomePage />} />
             {/*<Route path="/about" element={<About />} />*/}
             {/*<Route path="/login" element={<Login />} />*/}
             {/*<Route path="/register" element={<Register />} />*/}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import HeroSection from "../components/layout/HeroSection/HeroSection.tsx";
+
+export function HomePage() {
+  return (
+      <div>
+          <HeroSection/>
+          <section id="about" style={{padding: '20px 50px'}}>
+              <h2>About MingleFlix</h2>
+              <p>MingleFlix lets you create ephemeral rooms to watch videos with friends in real-time. Enjoy e2e
+                  encrypted chat and a shared viewing experience from anywhere!</p>
+          </section>
+      </div>
+  );
+}
