f492b58ea5796c4d7a67f339dac2b93e26a85992
alkl58 <alkl58@protonmail.com>
2024-04-23 13:30:56 +0200

    Fix prettier EOL (windows)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import './App.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Header from "./components/common/Header/Header.tsx";
-import {HomePage} from "./pages/HomePage.tsx";
-import {Login} from "./pages/Login.tsx";
-import {Register} from "./pages/Register.tsx";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/common/Header/Header.tsx';
+import { HomePage } from './pages/HomePage.tsx';
+import { Login } from './pages/Login.tsx';
+import { Register } from './pages/Register.tsx';
 
 function App() {
   return (
     <Router>
-        <Header />
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            {/*<Route path="/about" element={<About />} />*/}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-        </Routes>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        {/*<Route path="/about" element={<About />} />*/}
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
 
 const Header: React.FC = () => {
-    return (
-        <header style={{ display: 'flex', justifyContent: 'space-between', padding: '10px 50px', alignItems: 'center', backgroundColor: '#282c34', color: 'white' }}>
-            <h1>MingleFlix</h1>
-            <nav>
-                <a href="/about" style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}>About</a>
-                {/*<a href="/contact" style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}>Contact</a>*/}
-                <a href="/login" style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}>Login</a>
-                <a href="/register" style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}>Register</a>
-            </nav>
-        </header>
-    );
+  return (
+    <header
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '10px 50px',
+        alignItems: 'center',
+        backgroundColor: '#282c34',
+        color: 'white',
+      }}
+    >
+      <h1>MingleFlix</h1>
+      <nav>
+        <a
+          href='/about'
+          style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}
+        >
+          About
+        </a>
+        {/*<a href="/contact" style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}>Contact</a>*/}
+        <a
+          href='/login'
+          style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}
+        >
+          Login
+        </a>
+        <a
+          href='/register'
+          style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}
+        >
+          Register
+        </a>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
