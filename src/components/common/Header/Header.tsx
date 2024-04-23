b329c5cd9593ea8db3fc27b6c1b62e0ba13f8db6
alkl58 <alkl58@protonmail.com>
2024-04-23 15:58:08 +0200

    Update Login & Register Page

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -2,35 +2,21 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header
-      style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        padding: '10px 50px',
-        alignItems: 'center',
-        backgroundColor: '#282c34',
-        color: 'white',
-      }}
-    >
-      <h1>MingleFlix</h1>
+    <header className='flex text-white bg-[#292929] justify-between items-center p-3 border-b-[1px] border-sky-600'>
+      <h1 className='font-medium text-4xl'>
+        <a className='text-white' href='/'>
+          MingleFlix
+        </a>
+      </h1>
       <nav>
-        <a
-          href='/about'
-          style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}
-        >
+        <a href='/about' className='mr-[10px] text-white'>
           About
         </a>
         {/*<a href="/contact" style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}>Contact</a>*/}
-        <a
-          href='/login'
-          style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}
-        >
+        <a href='/login' className='mr-[10px] text-white'>
           Login
         </a>
-        <a
-          href='/register'
-          style={{ color: 'white', textDecoration: 'none', margin: '0 10px' }}
-        >
+        <a href='/register' className='mr-[10px] text-white'>
           Register
         </a>
       </nav>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,18 +1,53 @@
 export function Register() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h2>Register</h2>
-      <form>
-        <div className='flex gap-x-2'>
-          <label>Email:</label>
-          <input type='email' name='email' />
+    <div className='p-4 bg-neutral-900'>
+      <section className='h-screen'>
+        <div className='container h-full px-6'>
+          <div className='flex h-full flex-wrap items-center content-center justify-center'>
+            <div className='md:w-8/12 lg:ms-6 lg:w-[450px]'>
+              <form className='p-10 bg-[#292929] rounded-lg'>
+                <h2 className='text-4xl text-white p-5 pb-10'>Register</h2>
+                <div
+                  className='relative mb-6 bg-neutral-900 border-sky-600 hover:border-sky-400 transition duration-400 ease-in-out border-b-[2px] rounded-lg'
+                  data-twe-input-wrapper-init
+                >
+                  <input
+                    type='email'
+                    className='block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none text-white/80'
+                    id='email'
+                    placeholder='Email address'
+                  />
+                </div>
+
+                <div
+                  className='relative mb-6 bg-neutral-900 border-sky-600 hover:border-sky-400 transition duration-400 ease-in-out border-b-[2px] rounded-lg'
+                  data-twe-input-wrapper-init
+                >
+                  <input
+                    type='password'
+                    className='block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none text-white/80'
+                    id='password'
+                    placeholder='Password'
+                  />
+                </div>
+
+                <div className='mb-6 flex items-center justify-between float-right'>
+                  <a href='/login' className='text-white'>
+                    Already registered?
+                  </a>
+                </div>
+
+                <button
+                  type='submit'
+                  className='inline-block w-full rounded-lg bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal bg-sky-600 text-white shadow-primary-3 transition duration-400 ease-in-out hover:bg-sky-500 hover:shadow-primary-2 focus:bg-sky-500 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-sky-500 active:shadow-primary-2'
+                >
+                  Register
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className='flex gap-x-2'>
-          <label>Password:</label>
-          <input type='password' name='password' />
-        </div>
-        <button type='submit'>Register</button>
-      </form>
+      </section>
     </div>
   );
 }
