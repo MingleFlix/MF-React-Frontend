e35adc1ac51ee63c69f33c56b9a37f73839acee9
alkl58 <alkl58@protonmail.com>
2024-06-07 11:48:48 +0200

    Add player ambient mode

--- src/components/video/PlayerComponent.tsx
+++ src/components/video/PlayerComponent.tsx
@@ -1,4 +1,3 @@
-// PlyrVideoPlayer.tsx
 import React, { useRef, useEffect } from 'react';
 import Plyr from 'plyr';
 import 'plyr/dist/plyr.css';
@@ -15,17 +14,67 @@ interface PlayerComponentProps {
 }
 
 const PlyrVideoPlayer: React.FC<PlayerComponentProps> = ({ source }) => {
+  // Toggle player ambient mode
+  var ambientMode = true;
+
   const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const setCanvasDimension = (canvas: HTMLCanvasElement, video: HTMLVideoElement) => {
+    canvas.height = video.offsetHeight;
+    canvas.width = video.offsetWidth;
+  };
+
+  const paintStaticVideo = (ctx: CanvasRenderingContext2D, video: HTMLVideoElement) => {
+    if (!ambientMode) {
+      return;
+    }
+    ctx.drawImage(video, 0, 0, video.offsetWidth, video.offsetHeight);
+  };
 
   useEffect(() => {
+    // Init Player
     const player = new Plyr(videoRef.current!, {
       controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen']
     });
 
+    // Set Player video source
     player.source = source as Plyr.SourceInfo;
+
+    // Canvas for player ambient mode
+    const canvas = canvasRef.current;
+    const ctx = canvas!.getContext('2d');
+    // Our further logic requires a standard html5 video object
+    // Using the plyr player or the videoRef doesn't work
+    const video = document.getElementsByTagName('video')[0];
+
+    // Init Canvas
+    setCanvasDimension(canvas!, video);
+    paintStaticVideo(ctx!, video);
+
+    // When resizing the window we also need to resize the canvas
+    // Else it will float in space where it doesn't belong
+    const handleResize = () => {
+      setCanvasDimension(canvas!, video);
+      if (video.paused) {
+        paintStaticVideo(ctx!, video);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
 
     // Event Listener for play event
     player.on('play', () => {
+      setCanvasDimension(canvas!, video);
+
+      // Main loop for ambient mode
+      (function loop() {
+        if (!player.paused && !player.ended) {
+          ctx.drawImage(video, 0, 0, video.offsetWidth, video.offsetHeight);
+          setTimeout(loop, 24000 / 1001); // drawing at 23.976fps
+        }
+      })();
+      
       var currentTime = player.currentTime;
       console.log('Video started at ' + currentTime);
     });
@@ -40,6 +89,7 @@ const PlyrVideoPlayer: React.FC<PlayerComponentProps> = ({ source }) => {
     player.on('seeked', () => {
       var currentTime = player.currentTime;
       console.log('Video seeked to ' + currentTime);
+      paintStaticVideo(ctx!, video);
     });
 
     // Event Listener for timeupdate event
@@ -53,7 +103,8 @@ const PlyrVideoPlayer: React.FC<PlayerComponentProps> = ({ source }) => {
   }, [source]);
 
   return (
-    <div className=''>
+    <div>
+      <canvas ref={canvasRef} className="decoy"></canvas>
       <video ref={videoRef} className="plyr-react plyr" playsInline />
     </div>
   );
