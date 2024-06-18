7721d6eaf5c2df1e8a59804af810a2451aa29960
Jesse Guenzl <contact@jesseguenzl.com>
2024-06-18 14:49:21 +0200

    add shadcn ui lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-import { fontFamily } from 'tailwindcss/defaultTheme';
-
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
+module.exports = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: '',
   theme: {
     container: {
       center: true,
-      // padding: "2rem",
-      // screens: {
-      //   "2xl": "1400px",
-      // },
-    },
-    fontFamily: {
-      sans: ['var(--font-inter)', ...fontFamily.sans],
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
     },
     extend: {
       colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
@@ -27,29 +31,6 @@ export default {
           DEFAULT: 'hsl(var(--secondary))',
           foreground: 'hsl(var(--secondary-foreground))',
         },
-        // "off-white": "hsl(var(--card))",
-        // "off-white": "#FBFBFB",
-        yellow: {
-          // 200: "#FCF0C4", // beige
-          600: '#F8D600', // yellow light
-          // 700: "#EEC211", // yellow dark
-        },
-        gray: {
-          50: '#F6F6F6',
-          100: '#ECECEC',
-          200: '#CDCDCC',
-          300: '#B0B0AF',
-          400: '#868685',
-          500: '#757574',
-          600: '#575756',
-          900: '#20201E',
-        },
-        offwhite: '#FBFBFB',
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
           foreground: 'hsl(var(--destructive-foreground))',
@@ -69,14 +50,12 @@ export default {
         card: {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
-          footer: 'hsl(var(--card-footer))',
         },
       },
-      gridTemplateColumns: {
-        sidebar: '256px auto', //for sidebar layout
-      },
-      gridTemplateRows: {
-        header: '56px auto', //for the navbar layout
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
         'accordion-down': {
@@ -94,5 +73,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
