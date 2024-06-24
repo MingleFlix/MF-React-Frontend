48a082ae35bf914c4022bf04530b66c2a9ac5b57
alkl58 <alkl58@protonmail.com>
2024-06-24 21:42:00 +0200

    Video Input: switch to react-use-websocket

--- src/pages/Room/components/video/AddVideoInput.tsx
+++ src/pages/Room/components/video/AddVideoInput.tsx
@@ -1,6 +1,7 @@
 import { AuthContext } from '@/context/AuthContext';
 import { PlayerEvent } from '@/types/events';
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import React, { useState, useContext } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 const AddVideoInput: React.FC<{ roomId: string }> = ({ roomId }) => {
   // Auth
@@ -15,61 +16,43 @@ const AddVideoInput: React.FC<{ roomId: string }> = ({ roomId }) => {
   const token = auth ? auth.token : 'test-token';
 
   const [inputValue, setInputValue] = useState<string>('');
-  const ws = useRef<WebSocket | null>(null);
+
+  // Socket
+  const [socketUrl] = useState(
+    `/api/video-management?roomID=${roomId}&type=input&token=${token}`,
+  );
+
+  const { sendMessage, readyState } = useWebSocket(socketUrl);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
-
-  useEffect(() => {
-    // Create URL used for websocket
-    var webSocketUrl = new URL(
-      `/api/video-management?roomID=${roomId}&type=input&token=${token}`,
-      window.location.href,
-    );
-
-    // Need to switch protocol
-    webSocketUrl.protocol = webSocketUrl.protocol.replace('http', 'ws');
-
-    // Initialize WebSocket connection
-    ws.current = new WebSocket(webSocketUrl.href);
-
-    ws.current.onopen = () => {
-      console.log('WebSocket connection opened');
-    };
-
-    ws.current.onclose = () => {
-      console.log('WebSocket connection closed');
-    };
-
-    ws.current.onerror = error => {
-      console.error('WebSocket error', error);
-    };
-  }, []);
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key !== 'Enter') {
       return;
     }
 
-    if (ws.current && ws.current.readyState === WebSocket.OPEN) {
-      const playerEvent: PlayerEvent = {
-        room: roomId,
-        event: 'add-video',
-        user: user,
-        time: 0,
-        url: inputValue,
-      };
-
-      ws.current.send(JSON.stringify(playerEvent));
-      console.log('Sent to WebSocket:', inputValue);
-    } else {
-      console.error('WebSocket is not open');
+    if (readyState !== ReadyState.OPEN) {
+      alert('No connection to Video Sync Service!');
+      return;
     }
+
+    const playerEvent: PlayerEvent = {
+      room: roomId,
+      event: 'add-video',
+      user: user,
+      time: 0,
+      url: inputValue,
+    };
+
+    sendMessage(JSON.stringify(playerEvent));
   };
 
   return (
-    <div className='relative mb-4 bg-[#292929] border-sky-600 hover:border-sky-400 transition duration-400 ease-in-out border-b-[2px] rounded-lg sm:px-2 lg:px-4'>
+    <div
+      className={`relative mb-4 bg-[#292929] transition duration-400 ease-in-out border-b-[2px] rounded-lg sm:px-2 lg:px-4 ${readyState !== ReadyState.OPEN ? 'border-red-600 hover:border-red-400' : 'border-sky-600 hover:border-sky-400'}`}
+    >
       <input
         type='url'
         className='block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none text-white/80'
