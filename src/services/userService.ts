261e888c187851f0ebb709bfbc3830bc269b0733
Jesse Guenzl <contact@jesseguenzl.com>
2024-07-05 02:01:36 +0200

    add service to fetch user by id

---
+++ src/services/userService.ts
@@ -0,0 +1,22 @@
+import { UserWithRoles } from '@/types/user.ts';
+
+/**
+ * Fetches user data by ID.
+ * @param userId The ID of the user to fetch.
+ * @returns The user data.
+ */
+export async function fetchUserDataById(
+  userId: string,
+): Promise<UserWithRoles> {
+  try {
+    const response = await fetch(`/api/user-management/user/${userId}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const userData = await response.json();
+    return userData as UserWithRoles;
+  } catch (error) {
+    console.error('There was a problem fetching the user data:', error);
+    throw error;
+  }
+}
