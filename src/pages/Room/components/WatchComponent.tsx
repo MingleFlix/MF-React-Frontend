602cbfa5d1062b837ab2f371c8cda8221e8532b3
alkl58 <alkl58@protonmail.com>
2024-07-08 12:27:38 +0200

    Make Room page more responsive

--- src/pages/Room/components/WatchComponent.tsx
+++ src/pages/Room/components/WatchComponent.tsx
@@ -13,18 +13,16 @@ import { Chat } from '@/pages/Room/components/Chat.tsx';
 
 export function WatchComponent(props: { roomId: string }) {
   return (
-    <section id='about' className='h-[95vh]' style={{ padding: '0px 50px' }}>
+    <section id='about' className='h-[95vh] p-0 md:p-10'>
       <div className='h-[90vh] '>
-        <div className='flex flex-col gap-5 justify-center'>
-          <div className='flex gap-5'>
-            <div className='flex-1 max-w-[100%] xl:max-w-[90%] 2xl:max-w-[70%] min-w-[50%]'>
-              <PlyrPlayerComponent roomId={props.roomId} />
-            </div>
-            <div className='flex flex-col'>
-              <Chat roomId={props.roomId} />
-              <AddVideoInput roomId={props.roomId} />
-              <VideoQueueComponent roomId={props.roomId} />
-            </div>
+        <div className='flex flex-col gap-5 xl:flex-row'>
+          <div className='max-w-[100%] xl:max-w-[90%] 2xl:max-w-[70%] md:min-w-[50%]'>
+            <PlyrPlayerComponent roomId={props.roomId} />
+          </div>
+          <div className='flex flex-col'>
+            <Chat roomId={props.roomId} />
+            <AddVideoInput roomId={props.roomId} />
+            <VideoQueueComponent roomId={props.roomId} />
           </div>
         </div>
       </div>
